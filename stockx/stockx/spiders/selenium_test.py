922cb7f4bbc2c5779751d3c1a8d8a60a4e14cce3
Henry Blauth <henry.blauth@gmail.com>
2021-01-12 16:19:23 +0000

    svg extracted, try tooltip hover

--- stockx/stockx/spiders/selenium_test.py
+++ stockx/stockx/spiders/selenium_test.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 import time
-driver = webdriver.Chrome('/Users/henry/code/PATH/chromedriver')
+
+# Doesn't work with headless - think have to load javascript
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--headless')
+
+driver = webdriver.Chrome('/Users/henry/code/PATH/chromedriver', options=chrome_options)
 driver.get('https://stockx.com/crocs-classic-clog-justin-bieber')
 
 # Click to confim location & language
@@ -15,3 +20,11 @@ time.sleep(3)
 elements = driver.find_elements_by_class_name("highcharts-series.highcharts-series-0.highcharts-area-series > path")
 
 data = [element.get_attribute('d') for element in elements]
+
+driver.quit()
+
+# NB. Could also use driver.execute_script on these:
+# <script async data-chunk="components-pageSwitcherContainer" src="https://web-assets.stockx.com/highcharts.e50ee5dc.bundle.fb08dafb9f644a87006dbff70b135716f0c7b399.js"></script>
+# <script async data-chunk="components-pageSwitcherContainer" src="https://web-assets.stockx.com/common.a3608e05.bundle.fb08dafb9f644a87006dbff70b135716f0c7b399.js"></script>
+# <script async data-chunk="components-pageSwitcherContainer" src="https://web-assets.stockx.com/components-pageSwitcherContainer.e529af98.bundle.fb08dafb9f644a87006dbff70b135716f0c7b399.js"></script>
+
